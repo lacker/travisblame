03ec2c003cc243f5bbd169ae8bc8de967f5bfcb6
Kevin Lacker <lacker@gmail.com>
2017-02-15 17:09:32 -0800

    a next site for listing travis info

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+
+let ax = axios.create({
+  baseURL: 'https://api.travis-ci.org/v3/',
+  timeout: 20000,
+});
 
 export default class App extends React.Component {
   static async getInitialProps() {
+    // Find all the FB repos
+    let res = await ax.get(
+      'owner/facebook',
+      {params: {include: 'organization.repositories'}}
+    );
+    let repos = res.data.repositories.filter(repo => repo.active);
     return {
-      hello: 'world',
+      repos: repos,
     };
+  }
+
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
-      <div>hello {this.props.hello}</div>
+      <div>
+        {this.props.repos.map(repo => <div>{repo.slug}</div>)}
+      </div>
     );
   }
 }
