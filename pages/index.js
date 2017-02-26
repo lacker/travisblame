2d22d7b67a8cf3e9215b67b287482719fd4a5274
Kevin Lacker <lacker@gmail.com>
2017-02-25 20:58:55 -0800

    put todo to remind myself where to pick up

--- pages/index.js
+++ pages/index.js
@@ -120,6 +120,7 @@ export default class App extends React.Component {
   }
 
   componentWillMount() {
+    // TODO: only run this on the client. check if terminal errors gone
     this.loadAll();
   }
 
